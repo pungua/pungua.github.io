7befdfe3b69d6119e2451aaeacac025399ca17bc
pungua <95920431+pungua@users.noreply.github.com>
2025-11-07 16:42:54 +0900

    Add files via upload

---
+++ Homework_08/shFrag.glsl
@@ -0,0 +1,58 @@
+#version 300 es
+
+precision highp float;
+
+in vec3 fragPos;  
+in vec3 normal;  
+in vec2 texCoord;
+out vec4 FragColor;
+
+struct Material {
+    vec3 diffuse;       // surface's diffuse color
+    vec3 specular;      // surface's specular color
+    float shininess;    // specular shininess
+};
+
+struct Light {
+    //vec3 position;
+    vec3 direction;
+    vec3 ambient; // ambient 적용 strength
+    vec3 diffuse; // diffuse 적용 strength
+    vec3 specular; // specular 적용 strength
+};
+
+uniform Material material;
+uniform Light light;
+uniform vec3 u_viewPos;
+uniform float toonLevel;
+
+void main() {
+    // ambient
+    vec3 rgb = material.diffuse;
+    vec3 ambient = light.ambient * rgb;
+  	
+    // diffuse 
+    vec3 norm = normalize(normal);
+    //vec3 lightDir = normalize(light.position - fragPos);
+    vec3 lightDir = normalize(light.direction);
+    float dotNormLight = dot(norm, lightDir);
+    float diff = max(dotNormLight, 0.0);
+
+    // toon shading -> diff 양자화
+    diff = floor(diff*toonLevel)/toonLevel;
+
+    vec3 diffuse = light.diffuse * diff * rgb;  
+    
+    // specular
+    vec3 viewDir = normalize(u_viewPos - fragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = 0.0;
+    if (dotNormLight > 0.0) {
+        spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+        spec = floor(spec*toonLevel)/toonLevel;
+    }
+    vec3 specular = light.specular * spec * material.specular;  
+        
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
+} 
